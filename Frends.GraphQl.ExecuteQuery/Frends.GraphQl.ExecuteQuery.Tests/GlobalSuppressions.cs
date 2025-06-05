417f4f1d1aaf40368ad6ec27260f19015e1c3676
Mateusz Noga-Wojtania <mateusz.noga.wojtania@gmail.com>
2025-06-05 09:22:57 +0200

    renormalize crlf

--- Frends.GraphQl.ExecuteQuery/Frends.GraphQl.ExecuteQuery.Tests/GlobalSuppressions.cs
+++ Frends.GraphQl.ExecuteQuery/Frends.GraphQl.ExecuteQuery.Tests/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Following Frends documentation guidelines")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:FileMustHaveHeader", Justification = "Following Frends documentation guidelines")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1010:OpeningSquareBracketsMustBeSpacedCorrectly", Justification = "Incorrect in .net 8.0")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1000: The keyword 'new' should be followed by a space.", Justification = "Incorrect in .net 8.0")]
--- Frends.GraphQl.ExecuteQuery/Frends.GraphQl.ExecuteQuery/GlobalSuppressions.cs
+++ Frends.GraphQl.ExecuteQuery/Frends.GraphQl.ExecuteQuery/GlobalSuppressions.cs
@@ -8,3 +8,4 @@
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:FileMustHaveHeader", Justification = "Following Frends documentation guidelines")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1010:OpeningSquareBracketsMustBeSpacedCorrectly", Justification = "Incorrect in .net 8.0")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1206: The 'required' modifier should appear before 'public", Justification = "Incorrect in .net 8.0")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1000: The keyword 'new' should be followed by a space.", Justification = "Incorrect in .net 8.0")]
